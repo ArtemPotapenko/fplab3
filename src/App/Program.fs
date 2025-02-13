03ce55acd9ee30968b919228e168524a3de322f4
potap <336773@niuitmo.ru>
2025-02-13 20:21:28 +0300

    add App

---
+++ src/App/Program.fs
@@ -0,0 +1,11 @@
+﻿namespace fplab3
+
+open System
+open fplab3.lib.io.ConsoleInput
+open fplab3.service
+module App = 
+    [<EntryPoint>]
+    let main args =
+        let reader = Reader(Console.In)
+        MainService.execute (ArgsParserService.parse args) reader
+        0
