840cd0f25fcafa18e98e01d83a44c4959e93eaa7
potap <336773@niuitmo.ru>
2025-02-15 10:18:49 +0300

    add newton range

--- src/Library/method/NewtonInterpolation.fs
+++ src/Library/method/NewtonInterpolation.fs
@@ -1,15 +1,24 @@
 ﻿namespace fplab3.lib.method.NewtonInterpolation
 
 module NewtonInterpolation =
-    type NewtonState (x : double, y : double, result : double -> double, helpPoly : double -> double) =
+    type Point = {x : double
+                  y : double}
+    type NewtonState (x : double, y : double, result : double -> double, helpPoly : double -> double, count, set) =
         member this.result t =
             let a = (y - result x) / (helpPoly x)
             a * helpPoly t + result t
         member this.helpPoly t = (helpPoly t) * (t - x)
-        
-        member val point = (x, y)
-        
+        member this.count = count
+        member  this.point = { x = x;
+                             y = y }
+        member this.points = if count > 5 then (set |> Set.add this.point) else Set.empty
     
-    let create x y = NewtonState(x, y, (fun _ -> y), (fun _ -> 1.0))
+    let create x y = NewtonState(x, y, (fun _ -> y), (fun _ -> 1.0), 1, Set.empty)
     
-    let add x y (state : NewtonState) = NewtonState(x, y, state.result, state.helpPoly)
+    let rec add x y (state : NewtonState) =
+        if state.count = 9 then
+            let addPoint state point = add point.x point.y (state)
+            let newState = (state.points) |> Set.fold addPoint (create x y)
+            newState
+        else    
+            NewtonState(x, y, state.result, state.helpPoly, state.count + 1, state.points)
